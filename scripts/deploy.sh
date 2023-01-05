a1ff5946bc32d0f50eaf2dbec05d67433b98b869
JUN <chltmdwns96@naver.com>
2023-01-06 01:10:46 +0900

    dockerhub repository명 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,13 @@
 sudo docker ps -a -q --filter "name=app" | grep -q . && docker stop app && docker rm app | true
 
 # 기존 이미지 삭제
-sudo docker rmi pgmjun/docker-practice:latest
+sudo docker rmi pgmjun/cicdstudy:latest
 
 # 도커허브 이미지 pull
-sudo docker pull pgmjun/docker-practice:latest
+sudo docker pull pgmjun/cicdstudy:latest
 
 # 도커 run
-docker run -d -p 8080:8080 --name app pgmjun/docker-practice:latest
+docker run -d -p 8080:8080 --name app pgmjun/cicdstudy:latest
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
