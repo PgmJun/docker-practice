03b107ef6c71f02f23578850e7ab6621e12e50d0
JUN <chltmdwns96@naver.com>
2023-01-06 11:25:25 +0900

    env-file 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ sudo docker rmi pgmjun/cicdstudy:latest
 sudo docker pull pgmjun/cicdstudy:latest
 
 # 도커 run
-docker run -d -p 8080:8080 --name app pgmjun/cicdstudy:latest
+docker run -d -p 8080:8080 --env-file=env_list.txt --name app pgmjun/cicdstudy:latest
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
